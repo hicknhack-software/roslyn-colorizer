6fffe480de18ab35e014b9cf0ebf329112349a49
Vladislav Khapin <hapinvlad@gmail.com>
2018-05-13 17:12:03 +0300

    Returned field default color

--- SemanticColorizer/EditorFormats.cs
+++ SemanticColorizer/EditorFormats.cs
@@ -14,6 +14,7 @@ namespace SemanticColorizer
     {
         public SemanticFieldFormat() {
             DisplayName = "Semantic Field";
+            ForegroundColor = Colors.SaddleBrown;
         }
     }
 
